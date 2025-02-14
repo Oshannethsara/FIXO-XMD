5ce59ba7a3a39160410bcc83e1c691a063388fd5
fixoeditz <fixoeditz@gmail.com>
2025-02-14 20:15:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG  || "https://ibb.co/zWxZcLYL" ,
+ALIVE_IMG: process.env.ALIVE_IMG  || "https://ibb.co/zVZgCyyx" ,
 ALIVE_MSG: process.env.ALIVE_MSG || "HEY I AM FIXO XMD I AM ALIVE NOW",
 AUTO_READ_STATUS: process.env.AUTO_STATUS_READ ||"true",
 };
