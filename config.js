61a572ba91cde6b3c20bb15a80b84649caf20909
fixoeditz <fixoeditz@gmail.com>
2025-02-14 20:28:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG  || "https://ibb.co/zVZgCyyx" ,
 ALIVE_MSG: process.env.ALIVE_MSG || "HEY I AM FIXO XMD I AM ALIVE NOW",
 AUTO_READ_STATUS: process.env.AUTO_STATUS_READ ||"true",
+MODE: process.env.MODE || "public",
 };
