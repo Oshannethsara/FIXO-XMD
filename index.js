e0b88b97009649408d0cfb367f338645183104b1
fixoeditz <fixoeditz@gmail.com>
2025-02-16 08:15:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = '.'
+
 
 const ownerNumber = ['94711767300']
 
@@ -42,6 +42,10 @@ async function connectToWA() {
 //==============connect mongodb================
 const connectDB = require('./lib/mongodb')
 connectDB();
+//=============================================
+const {readEnv} = require('.lib/database')
+const config = await readEnv();
+const prefix = config.PREFIX
 //=============================================
 console.log("Connecting fixo xmd 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
