bd60f99324183753e7bd821eecebdeb4bcddf610
fixoeditz <fixoeditz@gmail.com>
2025-02-17 20:03:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID ||"",
-MONGODB: process.env.MONGODB ||"enter mongodb here",
+SESSION_ID: process.env.SESSION_ID ||"xr1VVJgL#4uq6prNDHOuI1ZqEEtx0va4Y02WcNWnEUi48mNsoX6A",
+MONGODB: process.env.MONGODB ||"mongodb://mongo:pPphhPWtIpFweBRlhKqmhigRvQlQkKsJ@switchyard.proxy.rlwy.net:43392",
 };
